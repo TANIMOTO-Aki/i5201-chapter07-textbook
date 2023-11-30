c505a9061b0f8257b4260e7efa62127d3af53421
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-11-30 10:02:32 +0900

    ０から９９までの乱数を発生させて、その数を当てるプログラムPractice7_1.javaを作成しました。 ついでに正解するまで入力した回数も表示するように勝手に付け足しました。 break文で抜け出す前提なので動作上問題は無いと思いますが、while文のもっといい書き方があれば教えていただけるとありがたいです。

---
+++ src/main/java/Practice7/Practice7_1.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class Practice7_1 {
+    public static void main(String[] args) throws IOException {
+        /* 入力の準備 */
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        /* 変数の宣言 */
+        int hitNumber;                                      //当てる数
+        int number;                                         //入力する数
+        int act = 0;                                        //入力回数カウンタ
+
+        hitNumber = (int)(Math.random()*100);   //乱数で当たりの数を設定
+        System.out.println("数当てゲーム(0~99までの数字です)");
+        System.out.println("何回で当てられるか挑戦してください");
+
+        /* 当たるまでループを繰り返す */
+        while (hitNumber <= 100) {
+            /* 数の入力 */
+            System.out.print("数:>");
+            number = Integer.parseInt(br.readLine());
+
+            /* 当たりの判定 */
+            if (number > hitNumber) {           //大き過ぎたとき
+                System.out.println(number + "より小さい値です");
+                act++;
+            } else if (number < hitNumber) {    //小さ過ぎたとき
+                System.out.println(number + "より大きい値です");
+                act++;
+            } else {                            //一致したとき
+                System.out.println(number + "で当たりです");
+                act++;
+                break;
+            }
+        }
+        System.out.println("数を当てるまでに入力した回数は " + act + "回 でした");
+        System.out.println("---プログラム終了---");
+    }
+}
